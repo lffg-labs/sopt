2e87a2be56b35cdd68b7a70744431bfac88b4484
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-02-03 17:04:49 -0300

    Create 2.js

---
+++ snippets/comment916460_493186/2.js
@@ -0,0 +1,30 @@
+const { readFile, writeFile } = require('fs').promises;
+
+async function addClient(filename, clientData) {
+  // Lê o arquivo e obtem o array atual:
+  const data = await readFile(filename, 'utf-8');
+  const json = JSON.parse((data || '').trim() || '[]');
+
+  if (!Array.isArray(json)) {
+    throw new Error(`Malformed JSON. Expected array, got: ${json}.`);
+  }
+
+  // Note que mutamos o JSON diretamente para "adicionar o cliente", uma vez que
+  // sobre-escreveremos o arquivo com o novo conteúdo (modificado).
+  json.push(clientData);
+
+  // Transformamos o JSON modificado em string para sobre-escrever no arquivo:
+  const jsonString = JSON.stringify(json);
+
+  // Modificamos o arquivo:
+  await writeFile(filename, jsonString);
+}
+
+// Posso chamar quantas vezes for necessário, a estrutura sintática do JSON
+// irá se manter válida.
+addClient('./data.json', {
+  name: 'Bob',
+  age: 10
+})
+  .then(() => console.log('OK.'))
+  .catch(console.error);
