d94683aa7dbdda6ad296dcb2e72c484d64d4637f
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-03-21 20:26:53 -0300

    Create dict-normalized-search.py

---
+++ snippets/500492/dict-normalized-search.py
@@ -0,0 +1,41 @@
+from unicodedata import normalize
+
+dicionario = {
+    'a': [],
+    'b': [],
+    'c': [],
+    'd': [],
+    'e': [],
+    'f': [],
+    'g': [],
+    'h': [],
+    'i': [],
+    'j': [],
+    'k': [],
+    'l': [],
+    'm': [],
+    'n': [],
+    'o': [],
+    'p': [],
+    'q': [],
+    'r': [],
+    's': [],
+    't': [],
+    'u': [],
+    'v': [],
+    'w': [],
+    'x': [],
+    'y': [],
+    'z': []}
+
+linha = input()
+
+for item in linha.split():
+    # Converte a letra para lowercase e "remove o acento":
+    letter = lower(normalize("NFD", item[0])[0])
+
+    if letter in dicionario:
+        if item not in dicionario[letter]:
+            dicionario[letter].append(item)
+
+print(dicionario)
