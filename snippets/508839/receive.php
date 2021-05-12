13451b5680d0ea591b2f6a5066a3a16720218467
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-05-12 12:15:31 -0300

    Create receive.php

---
+++ snippets/508839/receive.php
@@ -0,0 +1,16 @@
+<pre>
+<?php
+
+var_dump($_POST); // Imprime corretamente.
+
+?>
+</pre>
+<hr>
+<pre>
+<?php
+
+$idPlano = $_POST['id_plano'];
+print_r($idPlano); // Imprime corretamente.
+
+?>
+</pre>
