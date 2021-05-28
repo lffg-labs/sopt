14f61a2628914bbdd266350979b05335fc45d632
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-05-28 11:30:24 -0300

    Update unique-with-object-as-map.js

--- snippets/150413/unique-with-object-as-map.js
+++ snippets/150413/unique-with-object-as-map.js
@@ -1,3 +1,5 @@
+// src: https://pt.stackoverflow.com/a/150415/69296
+
 var array = [true, true, false, false, 1, 2, 3, 1, 2, 3, 'a', 'b', 'c', 'a'];
 var unique = array.reduce((acc, curr) => (acc[curr] = '', acc), {});
 
