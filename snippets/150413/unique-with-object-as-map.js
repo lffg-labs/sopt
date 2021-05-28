df99bf3052e70a23f2ec2a69aa01c578b662d88c
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-05-28 11:29:07 -0300

    Create unique-with-object-as-map.js

---
+++ snippets/150413/unique-with-object-as-map.js
@@ -0,0 +1,5 @@
+var array = [true, true, false, false, 1, 2, 3, 1, 2, 3, 'a', 'b', 'c', 'a'];
+var unique = array.reduce((acc, curr) => (acc[curr] = '', acc), {});
+
+// (8) ["1", "2", "3", "true", "false", "a", "b", "c"]
+console.log(Object.keys(unique));
